6d91c2e3d804e78fe3914bcbcac89504cc4f0dce
Pierre Kisters <pierrekisters@gmail.com>
2026-05-20 16:41:25 +0200

    WIP

--- circe/src/main/scala/de/lhns/filters4s/circe/package.scala
+++ circe/src/main/scala/de/lhns/filters4s/circe/package.scala
@@ -1,17 +1,31 @@
 package de.lhns.filters4s
 
+import de.lhns.filters4s.ast.{Const, ExprType}
+import io.circe.generic.semiauto.deriveCodec
+import io.circe.{Codec, Encoder, Json}
+
 package object circe {
-  /*implicit val constCodec: Codec[Const[_]] = Codec.from(
-    Decoder[Const[_]],
-    new Encoder[Const[_]] {
-      override def apply(const: Const[_]): Json =
-        const match {
-          case ExprType.NullType(()) => fr"NULL"
-          case ExprType.StringType(string) => fr"$string"
-          case ExprType.NumberType(number) => fr"$number"
-          case ExprType.BoolType(boolean) => if (boolean) fr"TRUE" else fr"FALSE"
-          case _ => throw new RuntimeException(s"unsupported db type: $value")
+  implicit val constCodec: Codec[Const[_]] = {
+    case class ConstJson(value: Json, `type`: String)
+    val constJsonCodec: Codec[ConstJson] = deriveCodec
+
+    Codec.from(
+      constJsonCodec.map { json =>
+        val value = json.`type` match {
+          case ExprType.NullType.name => Const((), ExprType.NullType)
+          case ExprType.StringType.name => Const(json.value.asString.get, ExprType.StringType)
         }
-    }
-  )*/
+      },
+      new Encoder[Const[_]] {
+        override def apply(const: Const[_]): Json =
+          const match {
+            case ExprType.NullType(()) => Json.Null
+            case ExprType.StringType(string) => Json.fromString(string.value)
+            case ExprType.NumberType(number) => Json.fromBigDecimal(number.value)
+            case ExprType.BoolType(boolean) => if (boolean.value) Json.True else Json.False
+            case _ => throw new RuntimeException(s"unsupported json type: ${const}")
+          }
+      }
+    )
+  }
 }
